4f0931ffcfda4a62ff1b6daea711465bd3f35b07
adams miracle <adamsmiracle0@gmail.com>
2024-08-02 01:51:44 +0000

    task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,7 @@
-export default function createEmployeesObject (departmentName, employees) {
-  const object = {
+export default function createEmployeesObject(departmentName, employees) {
+  return {
     [departmentName]: [
-      ...employees
-    ]
+      ...employees,
+    ],
   };
-  return object;
 }
