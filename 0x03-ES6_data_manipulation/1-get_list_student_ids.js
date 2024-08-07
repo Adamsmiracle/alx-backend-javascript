bc8379de3c46ffdcb2a7a08a79d44d3fe38384c0
adams miracle <adamsmiracle0@gmail.com>
2024-08-07 03:06:08 +0000

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,15 @@
-function getListStudentIds(students){
-    if (!Array.isArray(students)) {
-        return[]
-    }
-
-    return students.map(student => student.id)
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
